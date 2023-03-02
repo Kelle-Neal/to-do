ca3d4221bc1e7abe102237de1f165ff3f9bdf4cb
Kelle-Neal <kelle.neal@student.awesomeinc.org>
2023-03-02 20:57:19 +0000

    functional basic list

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [ tasks, setTasks ] = useState([]);
+  // const [ showTasks, setShowTasks ] = useState(false);
+    console.log(tasks);
+
+
+function AddTask({ setTasks, tasks }) {
+  const [ value, setValue ] = useState('');
+ 
+
+
+  function insertTask() {
+    console.log('clicked');
+
+
+    setTasks([...tasks, { text:value, id:Date.now() }]);
+    // setShowTasks(true);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input onChange={(e) => setValue(e.target.value)} type="textarea" />
+      <button onClick={insertTask}>Add it!</button>
+    </>
   );
 }
+
+
+function List({ tasks }) {
+  let tasksArray = tasks.map(task => {
+    return(
+      <>
+        <li key={task.id}>
+          <input type="checkbox" />
+          {task.text}
+          <button>X</button>
+        </li>
+      </>
+    );
+  })
+
+
+
+  return (
+      <ul>
+        {tasksArray}
+      </ul>
+    );
+}
+
+return (
+  <>      
+    <h1>Stuff I need to do...</h1>
+    <AddTask setTasks={setTasks} tasks={tasks} />
+    {/* <List tasks={tasks} /> */}
+    {tasks.length > 0 ? <List tasks={tasks} /> : <p>All caught up!</p>}
+    {/* {showTasks ? <p>We have some things to do...</p> : <p>All caught up!</p>} */}
+    <p>I have {tasks.length} things to get done...</p>
+
+  </>
+);
+}
+
+
 
 export default App;
